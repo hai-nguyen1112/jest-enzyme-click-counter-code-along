b1a61fdceedd6e2cf1e720d3687c9b4ed8820153
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-08-31 13:46:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+
   render() {
     return (
       <div data-test="component-app">
-        <h1>App</h1>
+        <h1 data-test="counter-display">The counter is {this.state.counter}</h1>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          data-test="increment-button"
+        >
+          Increment Counter
+        </button>
       </div>
     );
   }
